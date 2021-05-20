03201c9ef1fd3433017e08a8905d31be915726dc
ayaan-sayyed <84319719+ayaan-sayyed@users.noreply.github.com>
2021-05-20 13:38:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-function MY () {
-    document.getElementById("Avengers").src="https://images.news18.com/ibnlive/uploads/2019/03/Chris-Hemsworth-as-Thor-3.jpg" }
+function Setdate() {
+    document.getElementById('date').innerHTML= new Setdate().toDateString();
