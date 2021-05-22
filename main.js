204620b240b78720a4395a2d26b5432dba221c58
ayaan-sayyed <84319719+ayaan-sayyed@users.noreply.github.com>
2021-05-22 20:25:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-function MY () {
-    document.getElementById("Avngers").src="https://images.news18.com/ibnlive/uploads/2019/03/Chris-Hemsworth-as-Thor-3.jpg" }
+function my () {
+    document.getElementById("Avengers").src="https://images.news18.com/ibnlive/uploads/2019/03/Chris-Hemsworth-as-Thor-3.jpg" }
